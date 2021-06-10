63e17257f2af4e56aab03690d92a7b5b527a1f09
james <owsingames@yahoo.com>
2021-06-10 19:01:41 -0400

    Changed all list items to objects, added menu options

--- Lab8 - Student Database/Program.cs
+++ Lab8 - Student Database/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Lab8___Student_Database
 {
@@ -8,137 +9,223 @@ namespace Lab8___Student_Database
     {
         static void Main(string[] args)
         {
-            bool onContinue = true;
-            while(onContinue == true)
-            {
-                string message = GetUserInput("");
-                PersonIndex(message);
-                onContinue = PickAgain();
-            }    
+            //Student object list
+            List<Student> StudentInfo = new List<Student>();
+            StudentInfo.Add(new Student("Mark Haines", "Grand Rapids, MI", "Cilantro"));
+            StudentInfo.Add(new Student("Andrew Klima", "Grayslake, MI", "Sushi"));
+            StudentInfo.Add(new Student("Tommy Waalkes", "Raleigh, NC", "Chicken Curry"));
+            StudentInfo.Add(new Student("Maggie Tamanini", "Montrose, MI", "Movie Theater Popcorn"));
+            StudentInfo.Add(new Student("Jerome Brown", "Milwaukee, WI", "Italian cusine"));
+            StudentInfo.Add(new Student("Trent Nedbal", "Rochester MI", "Tacos"));
+            StudentInfo.Add(new Student("Troy Vizina", "Indian River, MI", "Broccoli"));
+            StudentInfo.Add(new Student("Kevin Jackson II", "Detroit, MI", "Asian cusine"));
+            StudentInfo.Add(new Student("Josh Carolin", "Northville MI", "Nalesniki"));
+            StudentInfo.Add(new Student("Kate Datema", "Zeeland, MI", "Pizza"));
+            StudentInfo.Add(new Student("James Moulton", "Toledo, OH", "Sushi"));
+
+            MainMenu(StudentInfo);
+         
+
+           
+
+
         }
-
-        public static string GetUserInput(string input)
+        public static void MainMenu(List<Student> StudentInfo)
         {
-            Console.WriteLine("Welcome to the student database");
-            Console.WriteLine("Please select a NUMBER to learn more about a person\n");
-            
-            Console.WriteLine("0) Mark");
-            Console.WriteLine("1) Andrew");
-            Console.WriteLine("2) Tommy");
-            Console.WriteLine("3) Maggie");
-            Console.WriteLine("4) Jerome");
-            Console.WriteLine("5) Trent");
-            Console.WriteLine("6) Troy");
-            Console.WriteLine("7) Kevin");
-            Console.WriteLine("8) Joshua");
-            Console.WriteLine("9) Sean");
-            Console.WriteLine("10) Kate");
-            Console.WriteLine("11) James");
-            Console.WriteLine();
+            Console.WriteLine("Would you like to see the Student list or Add a new Student");
+            Console.WriteLine("0) See student list");
+            Console.WriteLine("1) Add new Student \n");
 
-            string userInput = Console.ReadLine().ToLower().Trim();
-            
-            if(userInput == "")
+            string input = GetUserInput("Please select an option");
+            if (input == "0" || input == "see student list")
             {
-                Console.Clear();
-                Console.WriteLine("Please enter a number");
-                return GetUserInput(userInput);
+                //call student information list
+                StudentList(StudentInfo);
+            }
+            else if (input == "1" || input == "add a new student")
+            {
+                //call ad new student
+                Console.WriteLine("--Adding a new student--");
+                AddStudent(StudentInfo);
+
             }
             else
             {
+                Console.WriteLine("that is not an option");
+                GetUserInput("Please select an option provided");
+            }
+
+        }
+
+        public static void StudentList(List<Student> StudentInfo)
+        {
+            //print out list of all student objects by name
+            Console.WriteLine("--Student List--");
+            for(int i = 0; i < StudentInfo.Count; i++)
+            {
+                Student stud = StudentInfo[i];
+                Console.WriteLine($"{i} : {stud.Name}");
+            }
+
+            string input = GetUserInput("Would you like to Learn more about a person? (y/n)");
+            if (input == "yes" || input == "y")
+            {
+                DisplayAdditionInto(StudentInfo);
+            }
+            else if (input == "n" || input == "no")
+            {
                 Console.Clear();
+                MainMenu(StudentInfo);
+            }
+        }
+
+        public static void DisplayAdditionInto(List<Student> StudentInfo)
+        {
+            Console.WriteLine("Please select the index of the student you want to learn more about");
+            string input = Console.ReadLine();
+            try
+            {
+                int index = int.Parse(input);
+                
+                for(int i = 0; i < StudentInfo.Count; i++)
+                {
+                    Student stud = StudentInfo[i];
+                    if (index == i)
+                    {
+                        Console.WriteLine($"Favorite food is: {stud.FavoriteFood}");
+                        Console.WriteLine($"Hometown is: {stud.Hometown}");
+                        Console.ReadLine();
+                        MainMenu(StudentInfo);
+                    }
+
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please select the index of the student you want to learn more about");
+                DisplayAdditionInto(StudentInfo);
+            }
+
+        }
+
+        public static void DisplayStudentList()
+        {
+            //relative file path -- file is in bin folder
+            string filePath = "StudentInfo.txt";
+            //stream reader object -- will read file
+            StreamReader reader = new StreamReader(filePath);
+            //read file until end of file
+            string output = reader.ReadToEnd();
+            Console.WriteLine(output);
+
+            //split lines of file when new line starts, then store those new lines in and array
+            string[] lines = output.Split("\n");
+            //create a list of student objects
+            List<Student> studentInfo = new List<Student>();
+
+            //convert each line into a Student object 
+            foreach (string line in lines)
+            {
+                Student stud = ConvertToStudent(line);
+                if (stud != null)
+                {
+                    //add new converted student object to List of Students
+                    studentInfo.Add(stud);
+                }
+            }
+            reader.Close();
+        }
+
+        public static void AddStudent(List<Student> StudentInfo)
+        {
+            //access to file path -- working with this file
+            string filePath = "StudentInfo.txt";
+
+            //create a new student object
+            Student s = new Student();
+
+            //ask user for information
+            Console.WriteLine("Please enter student name: ");
+            s.Name = Console.ReadLine();
+
+            Console.WriteLine("Please enter Hometown:");
+            s.Hometown = Console.ReadLine();
+
+            Console.WriteLine("Please enter Favoite food:");
+            s.FavoriteFood = Console.ReadLine();
+
+            //convert Student object to string format
+            string line = StudentToString(s);
+            StudentInfo.Add(s);
+            Console.Write($"{line} has been added to StudentInfo list");
+
+            //read file
+            StreamReader reader = new StreamReader(filePath);
+            //make copy of original txt file
+            string original = reader.ReadToEnd();
+            reader.Close();
+
+            //write new info to existing file
+            StreamWriter writer = new StreamWriter(filePath);
+            writer.WriteLine(original + line);
+            writer.Close();
+            
+
+        }
+
+        public static string StudentToString(Student s)
+        {
+            string output = $"{s.Name}, {s.Hometown}, {s.FavoriteFood}";
+            return output;
+        }
+
+        public static Student ConvertToStudent(string line)
+        {
+            //split the line at every comma and store it into a string array
+            string[] properties = line.Split(',');
+            //create a new student object
+            Student s = new Student();
+
+            //check it new string has 3 words
+            if (properties.Length == 3)
+            {
+                //store the name as the name property from index 0
+                s.Name = properties[0];
+                //store the hometown as the hometown property from index 1
+                s.Hometown = properties[1];
+                //store the favorite food as the favorite food property from index 2
+                s.FavoriteFood = properties[2];
+                //return new object with properties
+                return s;
+            }
+            else
+            {
+                //return empty object
+                return s;
+            }
+        }
+
+        public static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            string userInput = Console.ReadLine().ToLower().Trim();
+
+            if (userInput == "0" || userInput == "1")
+            {
                 return userInput;
             }
-
-           
-        }
-
-        public static void PersonIndex(string input)
-        {
-            Console.Clear();
-
-            //list of names
-            List<string> names = new List<string>();
-            names.Add("Mark Haines"); //food: Cilantro -- hometown: Grand Rapids MI
-            names.Add("Andrew Klima"); //food: sushi -- hometown: Grayslake MI
-            names.Add("Tommy Waalkes"); //food: Chicken curry -- hometown: Raleigh NC
-            names.Add("Maggie Tamanini"); //food: Movie theater popcorn -- hometown: Montrose Mi
-            names.Add("Jerome Brown"); //food: Italian cusine -- hometown: Milwaukee WI
-            names.Add("Trent Nedbal"); //food: tacos -- hometown: Rochester MI
-            names.Add("Troy Vizina"); //food: Broccoli -- hometown: Indian River MI
-            names.Add("Kevin Jackson II"); //food: Asian cusine -- hometown: Detroit MI
-            names.Add("Joshua Carolin"); //food: Nalesniki -- hometown: Northville MI
-            names.Add("Sean Boatman"); //food: MEAT! -- hometown: Eaton Rapids MI
-            names.Add("Kate Datema"); //food: Pizza -- hometown: Zeeland MI
-            names.Add("James Moulton"); //food: sushi -- Hometown: Toledo OH
-
-            int index = int.Parse(input);
-            string name = names[index];
-
-            if (name == names[index])
+            else if (userInput == "y" || userInput == "yes")
             {
-                string fullname = names[index];
-                Console.WriteLine("Full name is: " + fullname);
-                FoodIndex(index.ToString());
+                return userInput;
             }
-        }
-
-        public static void FoodIndex(string input)
-        {
-            //list of favorite foods
-            //index number is same as Name and hometown
-            List<string> foods = new List<string>();
-            foods.Add("Cilantro");
-            foods.Add("Sushi");
-            foods.Add("Chicken Curry");
-            foods.Add("Movie Theater Popcorn");
-            foods.Add("Italian Cusine");
-            foods.Add("Tacos");
-            foods.Add("Broccoli");
-            foods.Add("Asian Cusine");
-            foods.Add("Nalesniki");
-            foods.Add("MEAT!");
-            foods.Add("Pizza");
-            foods.Add("Sushi");
-
-            int index = int.Parse(input);
-            string food = foods[index];
-
-            if (food == foods[index])
+            else if (userInput == "n" || userInput == "no")
             {
-                string favoriteFood = foods[index];
-                Console.WriteLine("Favorite food is: " + favoriteFood);
-                HometownIndex(index.ToString());
+                return userInput;
             }
-        }
-
-        public static void HometownIndex(string input)
-        {
-            //list of hometowns
-            //index is same/related to name and food
-            List<string> hometown = new List<string>();
-            hometown.Add("Grand Rapids, MI");
-            hometown.Add("GrayLake, MI");
-            hometown.Add("Raleigh, NC");
-            hometown.Add("Montrose MI");
-            hometown.Add("Milwaukee, WI");
-            hometown.Add("Rochester, MI");
-            hometown.Add("Indian River, MI");
-            hometown.Add("Detroit, MI");
-            hometown.Add("Northville MI");
-            hometown.Add("Eaton Rapids, MI");
-            hometown.Add("ZeeLand, MI");
-            hometown.Add("Toledo, OH");
-
-            int index = int.Parse(input);
-            string home = hometown[index];
-
-            if (home == hometown[index])
+            else
             {
-                string ht = hometown[index];
-                Console.WriteLine("Hometown is: " + ht);
-                
-            }
+                return GetUserInput("Please select an option provided");
+            } 
         }
 
         public static bool PickAgain()
---
+++ Lab8 - Student Database/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab8___Student_Database
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string Hometown { get; set; }
+        public string FavoriteFood { get; set; }
+
+        public Student()
+        {
+
+        }
+
+        public Student(string Name, string Hometown, string FavoriteFood)
+        {
+            this.Name = Name;
+            this.Hometown = Hometown;
+            this.FavoriteFood = FavoriteFood;
+        }
+        
+    }
+}
